a705e4cddb36b3655ff8898997a91588e9654908
Kadono Yumiko <ykadono@ecc.ac.jp>
2022-08-10 11:10:14 +0900

    CSSファイルを相対パスに変更

--- resources/views/layouts/kadai.blade.php
+++ resources/views/layouts/kadai.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-    <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+    <link href="css/app.css" rel="stylesheet">
 
     <title>@yield('pageTitle') - サーバーサイドスクリプト演習２</title>
 </head>
